bb6d72518ec618d788515dc94b75d6f236cc3260
alanssitis <achungma@purdue.edu>
2023-09-05 11:18:17 -0400

    change settings to make it work on website
    site and base settings modified to work on the website.
    
    updated a few links

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://engineering.purdue.edu',
+  base: '/477grp5',
   integrations: [tailwind()]
 });
