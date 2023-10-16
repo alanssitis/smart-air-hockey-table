7fd74aa2bb05bce9f7802bc05a5e1150909291c1
Trevor M <moorman2001@hotmail.com>
2023-10-16 14:12:33 -0400

    Re-implemented StateMachine
    Created app_statemachine.c and app_statemachine.h

---
+++ firmware-nucleo/smart-air-hockey-table/App/Inc/app_statemachine.h
@@ -0,0 +1,35 @@
+#ifndef INC_APP_STATEMACHINE_H_
+#define INC_APP_STATEMACHINE_H_
+
+#include <inttypes.h>
+
+#define NUM_PLAYERS 2
+#define PLAYER_A_IDX 0
+#define PLAYER_B_IDX 1
+
+/* TODO:
+ * May be able to combine states for each player (e.g. GameState_StartAnimationA & GameState_StartAnimationB)
+ * Will know whether or not once we begin work on the states' handling function.
+ */
+typedef enum _GameState {
+	GameState_SetUp,			// Initial set-up of the table
+	GameState_Idle,				// Idle before start of the game
+	GameState_Sleep,			// Low-power mode after inactivity
+	GameState_StartGame,		// Set-up sequence for new game
+	GameState_StartAnimationA,	// Playing start animation for player A
+	GameState_StartAnimationB,	// Playing start animation for player B
+	GameState_RunGame,			// Game is in progress
+	GameState_ScoreAnimationA,	// Player A scored (not the winning shot)
+	GameState_ScoreAnimationB,	// Player B scored (not the winning shot)
+	GameState_WinAnimationA,	// Player A won the game (winning shot)
+	GameState_WinAnimationB,	// Player B won the game (winning shot)
+	GameState_Count				// State used for boundary checking
+} GameState;
+
+GameState App_StateMachine_GetGameState();
+uint8_t App_StateMachine_GetWinScore();
+uint8_t App_StateMachine_GetPlayerScore(uint8_t playerIdx);
+void App_StateMachine_IncrementPlayerScore(uint8_t playerIdx);
+void App_StateMachine_GameTick();
+
+#endif /* INC_APP_STATEMACHINE_H_ */
---
+++ firmware-nucleo/smart-air-hockey-table/App/app_statemachine.c
@@ -0,0 +1,77 @@
+#include "app_statemachine.h"
+
+static volatile GameState currGameState;
+// TODO: Make winScore modifiable by the player through the OLED menu, for now it is hard-coded to 7
+static volatile uint8_t winScore = 7; // Number of points required for a player to win
+static volatile uint8_t playerScores[NUM_PLAYERS];
+
+GameState App_StateMachine_GetGameState()
+{
+	return currGameState;
+}
+
+uint8_t App_StateMachine_GetWinScore()
+{
+	return winScore;
+}
+
+uint8_t App_StateMachine_GetPlayerScore(uint8_t playerIdx)
+{
+	if (playerIdx < NUM_PLAYERS)
+	{
+		return playerScores[playerIdx];
+	}
+}
+
+void App_StateMachine_IncrementPlayerScore(uint8_t playerIdx)
+{
+	if (playerIdx < NUM_PLAYERS)
+	{
+		playerScores[playerIdx] += 1;
+	}
+}
+
+// Super-loop that is called every TIM7 tick (1ms)
+void App_StateMachine_GameTick()
+{
+	//  Coordinating switch statement, each case calls a function which will handle the current case and return the next case
+	switch (currGameState)
+	{
+		case (GameState_SetUp):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_Idle):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_Sleep):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_StartGame):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_StartAnimationA):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_StartAnimationB):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_RunGame):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_ScoreAnimationA):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_ScoreAnimationB):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_WinAnimationA):
+			// TODO: Implement state handling function
+			break;
+		case (GameState_WinAnimationB):
+			// TODO: Implement state handling function
+			break;
+		default:
+			// Error: default should occur
+			break;
+	}
+}
