b6496ba01305010ece040ac0de3f9f9fc165a5af
William Dobert <wdobert@purdue.edu>
2023-10-16 09:21:37 -0400

    Delete old firmware

--- firmware/smart-air-hockey-table/App/Inc/app_core.h
+++
@@ -1,6 +0,0 @@
-#ifndef INC_APP_CORE_H_
-#define INC_APP_CORE_H_
-
-void App_Init();
-
-#endif /* INC_APP_CORE_H_ */
--- firmware/smart-air-hockey-table/App/Inc/app_debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef INC_APP_DEBUG_H_
-#define INC_APP_DEBUG_H_
-
-void Debug_Init();
-void Debug_Log(const char* format, ...);
-void TIM4_Handler();
-
-#endif /* INC_APP_DEBUG_H_ */
