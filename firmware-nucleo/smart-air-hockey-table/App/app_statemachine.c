6a330726e16fec46793c88d166edaadddc4fcfea
Trevor Moorman <tmoorma@purdue.edu>
2023-10-16 20:25:06 -0400

    Created struct containing game info

--- firmware-nucleo/smart-air-hockey-table/App/app_statemachine.c
+++ firmware-nucleo/smart-air-hockey-table/App/app_statemachine.c
@@ -1,48 +1,17 @@
 #include "app_statemachine.h"
 
-static volatile GameState currGameState = GameState_Error;
-// TODO: Make winScore modifiable by the player through the OLED menu, for now it is hard-coded to 7
-static volatile uint8_t winScore = 7; // Number of points required for a player to win
-static volatile uint8_t playerScoreA; // Current number of points Player A has scored
-static volatile uint8_t playerScoreB; // Current number of points Player B has scored
-
-GameState App_StateMachine_GetGameState()
-{
-	return currGameState;
-}
-
-uint8_t App_StateMachine_GetWinScore()
-{
-	return winScore;
-}
-
-// If an invalid PlayerId is provided, then UINT8_MAX is returned
-uint8_t App_StateMachine_GetPlayerScore(PlayerId id)
-{
-	if (id == PlayerId_A) {
-		return playerScoreA;
-	}
-	else if (id == PlayerId_B) {
-		return playerScoreB;
-	}
-
-	return UINT8_MAX;
-}
-
-void App_StateMachine_IncrementPlayerScore(PlayerId id)
-{
-	if (id == PlayerId_A) {
-		playerScoreA += 1;
-	}
-	else if (id == PlayerId_B) {
-		playerScoreB += 1;
-	}
-}
+static volatile struct currGameInfo {
+	GameState currGameState = GameState_Error;
+	// TODO: Make winScore modifiable by the player through the OLED menu, for now it is hard-coded to 7
+	uint8_t winScore = 7; // Number of points required for a player to win
+	uint8_t playerScoreA = 0; // Current number of points Player A has scored
+	uint8_t playerScoreB = 0; // Current number of points Player B has scored
+};
 
 // Super-loop that is called every TIM7 tick (1ms)
 void App_StateMachine_GameTick()
 {
-	//  Coordinating switch statement, each case calls a function which will handle the current case and return the next case
+	//  Coordinating switch statement, each case calls a function which will handle currGameInfo
 	switch (currGameState)
 	{
 		case (GameState_SetUp):
