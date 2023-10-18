4880b3fbd58c5228e6b265c75e3d686c154da4f2
William Dobert <wdobert@purdue.edu>
2023-10-18 14:57:56 -0400

    Implement display power control

--- firmware-nucleo/smart-air-hockey-table/App/driver_display.c
+++ firmware-nucleo/smart-air-hockey-table/App/driver_display.c
@@ -79,13 +79,8 @@ void Driver_Display_Init()
 
 	transmit_end();
 
-	// Clear GDDRAM
 	Driver_Display_Clear(DISPLAY_ALL);
-
-	// Display on
-	transmit_start(DISPLAY_ALL, false);
-	transmit_word(0xAF);
-	transmit_end();
+	Driver_Display_SetPowered(DISPLAY_ALL, true);
 }
 
 void Driver_Display_Clear(Display display)
@@ -94,6 +89,14 @@ void Driver_Display_Clear(Display display)
 
 	transmit_start(display, true);
 	for (size_t i = 0; i < DISPLAY_WIDTH * DISPLAY_HEIGHT / 8; i++) transmit_word(0x00);
+	transmit_end();
+}
+
+void Driver_Display_SetPowered(Display display, bool is_powered)
+{
+	// Controls power to the OLED panel, the driver IC is always on
+	transmit_start(display, false);
+	transmit_word(is_powered ? 0xAF : 0xAE);
 	transmit_end();
 }
 
