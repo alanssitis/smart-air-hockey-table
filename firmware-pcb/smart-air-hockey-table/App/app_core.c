7cc49fb3a2629ef5aeb51350629fd37d6f44b812
Moorman <tmoorma@purdue.edu>
2023-11-07 14:51:59 -0500

    Revert app_core.c to match nucleo for merging into main

--- firmware-pcb/smart-air-hockey-table/App/app_core.c
+++ firmware-pcb/smart-air-hockey-table/App/app_core.c
@@ -1,8 +1,6 @@
 #include "app_core.h"
 
-#include <stdbool.h>
 #include "stm32u5xx_ll_tim.h"
-#include "stm32u5xx_ll_gpio.h"
 #include "stm32u5xx_ll_utils.h"
 
 #include "app_statemachine.h"
@@ -27,9 +25,7 @@ void App_Init()
 	Driver_Encoder_Init();
 
 	// Last step: set initial state
-	//App_StateMachine_Init();
-
-	Driver_Display_Print(DISPLAY_ALL, 0, 0, "\tSmart Air Hockey Table");
+	App_StateMachine_Init();
 
 	// Start 1000 Hz "superloop"
 //	LL_TIM_SetUpdateSource(TIM6, LL_TIM_UPDATESOURCE_COUNTER);
@@ -54,46 +50,7 @@ void TIM7_Handler()
 //		// Doesn't do much yet, but we can detect when ticks are missed
 //	}
 
-	//App_StateMachine_GameTick();
-
-/*
-	static uint_fast32_t rotation;
-	static bool is_pressed;
-
-	if (Driver_Encoder_PollButton())
-	{
-		is_pressed = !is_pressed;
-		if (is_pressed) Driver_Encoder_PollRotation();
-	}
-
-	LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
-	if (is_pressed) {
-		rotation += Driver_Encoder_PollRotation();
-		LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_0 << (rotation % 4));
-	}
-*/
-	Driver_Goal_Poll();
-	ldr1_goal ? LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0) : LL_GPIO_SetOutputPin(GPIOC, LL_GPIO_PIN_0);
-
-	static uint_fast32_t score_a, score_b;
-	if (ticks_completed % 500 == 0) score_a++;
-	if (ticks_completed % 900 == 0) score_b++;
-	if (ticks_completed % 500 == 0 || ticks_completed % 900 == 0)
-	{
-		Driver_Display_ShowScore(DISPLAY_0, score_a % 100, score_b % 100);
-		Driver_Display_ShowScore(DISPLAY_1, score_b % 100, score_a % 100);
-	}
-
-	Driver_HallEffect_PollInputs();
-
-	if ((halleffect_cols & 0b01) && (halleffect_rows & 0b01)) Driver_LED_SetColor(0, 0, (Color) { 0x77, 0x77, 0x77 });
-	else Driver_LED_SetColor(0, 0, (Color) { 0x00, 0x00, 0x00 });
-	if ((halleffect_cols & 0b01) && (halleffect_rows & 0b10)) Driver_LED_SetColor(0, 1, (Color) { 0x77, 0x77, 0x77 });
-	else Driver_LED_SetColor(0, 1, (Color) { 0x00, 0x00, 0x00 });
-	if ((halleffect_cols & 0b10) && (halleffect_rows & 0b01)) Driver_LED_SetColor(1, 0, (Color) { 0x77, 0x77, 0x77 });
-	else Driver_LED_SetColor(1, 0, (Color) { 0x00, 0x00, 0x00 });
-	if ((halleffect_cols & 0b10) && (halleffect_rows & 0b10)) Driver_LED_SetColor(1, 1, (Color) { 0x77, 0x77, 0x77 });
-	else Driver_LED_SetColor(1, 1, (Color) { 0x00, 0x00, 0x00 });
+	App_StateMachine_GameTick();
 
 	Driver_LED_Tick();
 
